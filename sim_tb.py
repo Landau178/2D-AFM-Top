a106b109186f1e0e4397790e88b3ddc3e20cab3a
Landau178 <vbjaeschke@gmail.com>
2020-09-07 17:36:21 -0300

    Simulation_TB doc

--- sim_tb.py
+++ sim_tb.py
@@ -2,7 +2,18 @@
 
 class Simulation_TB():
     """
+    This class perform calculations of tight-binding models,
+    using the library PythTB. To be initialized, it needs a
+    path to the simulation directory, which inside contains a
+    config file path/config.json, with some options for initialization.
+
+    config.json
+    -----------
+        dim_k: (int)
+        dim_r: (dim_r)
+
+
     """
 
-    def __init__(self, dir):
-        pass
+    def __init__(self, path):
+        self.dir = path
