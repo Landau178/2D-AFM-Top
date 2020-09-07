57e2bad3ba424e89f1b785834a43dceb03e4fb8f
Landau178 <vbjaeschke@gmail.com>
2020-09-07 17:33:06 -0300

    old_project changes of names

--- old_project/simulation_tb_old.py
+++ old_project/simulation_tb_old.py
@@ -7,7 +7,6 @@ import tb_utilities as tbu
 import os
 
 
-
 class Simulation_TB():
     def __init__(self, dir):
         [t, lamb, h] = np.loadtxt(dir + 'parameters.dat')
@@ -31,7 +30,7 @@ class Simulation_TB():
         self.n_spin = 2
         self.n_bands = self.n_orb * self.n_spin
 
-        h = np.array([0,0, h])
+        h = np.array([0, 0, h])
         sq3 = np.sqrt(3)
         a1 = np.array([1, 0, 0])
         a2 = np.array([-1/2, sq3/2, 0])
@@ -40,22 +39,23 @@ class Simulation_TB():
         l2 = tbu.vector_SU2(0.5 * a1 + 0.5 * a2)
         l3 = tbu.vector_SU2(0.5 * a2)
         lat = [list(a1), list(a2), list(a3)]
-        orb = [[1/3, 1/6, 0], # spin up
-            [5/6, 1/6, 0],  # spin down
-            [1/3, 2/3, 0],  # spin down
-            [5/6, 2/3, 0]] # spin up
+        orb = [[1/3, 1/6, 0],  # spin up
+               [5/6, 1/6, 0],  # spin down
+               [1/3, 2/3, 0],  # spin down
+               [5/6, 2/3, 0]]  # spin up
         tb_model = pytb.tb_model(2, 3, lat=lat, orb=orb, nspin=2)
         # nearest neighbours hoppings
         t = t * np.identity(2)
         sigma_z = np.array([[1, 0], [0, -1]])
         h = tbu.vector_SU2(h)
         alpha = sq3/24 * lamb
-        beta = lamb /36
+        beta = lamb / 36
         tb_model.set_hop(t + 1j*alpha * sigma_z + 1j*beta*l1, 0, 1, [0, 0, 0])
         tb_model.set_hop(t + 1j*alpha * sigma_z + 1j*beta*l3, 0, 2, [0, 0, 0])
         tb_model.set_hop(t - 1j*alpha * sigma_z + 1j*beta*l2, 0, 3, [0, 0, 0])
         tb_model.set_hop(t - 1j*alpha * sigma_z - 1j*beta*l1, 0, 1, [-1, 0, 0])
-        tb_model.set_hop(t + 1j*alpha * sigma_z - 1j*beta*l2, 0, 3, [-1, -1, 0])
+        tb_model.set_hop(t + 1j*alpha * sigma_z - 1j *
+                         beta*l2, 0, 3, [-1, -1, 0])
         tb_model.set_hop(t - 1j*alpha * sigma_z - 1j*beta*l3, 0, 2, [0, -1, 0])
 
         tb_model.set_hop(t + 1j*alpha * sigma_z + 1j*beta*l3, 1, 3, [0, 0, 0])
@@ -70,40 +70,40 @@ class Simulation_TB():
         return tb_model
 
     def plot_bands_path(self):
-        path=[[0.,0.],[1./3.,1./3.],[0.5,0],[2/3.,-1/3.], [0.,0.]]
+        path = [[0., 0.], [1./3., 1./3.], [0.5, 0], [2/3., -1/3.], [0., 0.]]
         # labels of the nodes
-        label=(r'$\Gamma $',r'$K$', r'$M$', r'$K^\prime$', r'$\Gamma $')
+        label = (r'$\Gamma $', r'$K$', r'$M$', r'$K^\prime$', r'$\Gamma $')
 
         # call function k_path to construct the actual path
-        (k_vec,k_dist,k_node) = self.model.k_path(path,301)
+        (k_vec, k_dist, k_node) = self.model.k_path(path, 301)
         evals = self.model.solve_all(k_vec)
-        #Plot of bands
+        # Plot of bands
         fig, ax = plt.subplots()
-        ax.set_xlim(k_node[0],k_node[-1])
+        ax.set_xlim(k_node[0], k_node[-1])
         ax.set_xticks(k_node)
         ax.set_xticklabels(label)
         for n in range(len(k_node)):
-          ax.axvline(x=k_node[n],linewidth=0.5, color='k')
+            ax.axvline(x=k_node[n], linewidth=0.5, color='k')
         ax.set_xlabel("Path in k-space")
         ax.set_ylabel("Band energy")
         for i in range(self.n_bands):
-            ax.plot(k_dist,evals[i])
+            ax.plot(k_dist, evals[i])
         fig_name = 'bands_k_path.png'
         dir = self.dir + 'images/' + fig_name
         fig.savefig(dir, dpi=500)
         plt.close()
 
     def eigen_functions_grid(self, n_step=100):
-        w_square=pytb.wf_array(self.model,[n_step,n_step])
-        all_kpt=np.zeros((n_step,n_step,2))
+        w_square = pytb.wf_array(self.model, [n_step, n_step])
+        all_kpt = np.zeros((n_step, n_step, 2))
         bands = np.zeros((n_step, n_step, self.n_bands))
         all_evec = np.zeros((n_step, n_step, self.n_bands, self.n_orb,
-                self.n_spin), dtype='complex')
+                             self.n_spin), dtype='complex')
         for i in range(n_step):
             for j in range(n_step):
-                kpt= np.array([i, j]) / n_step
-                all_kpt[i,j,:]=kpt
-                (eval,evec)=self.model.solve_one(kpt,eig_vectors=True)
+                kpt = np.array([i, j]) / n_step
+                all_kpt[i, j, :] = kpt
+                (eval, evec) = self.model.solve_one(kpt, eig_vectors=True)
                 bands[i, j, :] = eval
                 w_square[i, j] = evec
                 all_evec[i, j, :, :] = evec
@@ -124,7 +124,7 @@ class Simulation_TB():
     def load_grid(self, n_step):
         str_step = str(n_step) + 'x' + str(n_step)
         grid_folder = 'regular_grids/' + str_step
-        w_square=pytb.wf_array(self.model,[n_step,n_step])
+        w_square = pytb.wf_array(self.model, [n_step, n_step])
         self.k_grid = np.load(self.dir + grid_folder + '/k_grid.npy')
         self.bands = np.load(self.dir + grid_folder + '/evals.npy')
         self.evecs = np.load(self.dir + grid_folder + '/evecs.npy')
@@ -132,5 +132,3 @@ class Simulation_TB():
             for j in range(n_step):
                 w_square[i, j] = self.evecs[i, j, :, :, :]
         self.wf = w_square
-
-    def plot_bands_colormap()
--- old_project/tb_model_CrAs2.py
+++ old_project/tb_model_CrAs2.py
@@ -7,20 +7,20 @@ e_Cr_xy = 0.8
 e_Cr_x2_y2 = 0.9
 
 h_aa = np.array([[0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 1.]])
+                 [0, 0, 0],
+                 [0, 0, 1.]])
 h_ca_1u = np.array([[0.5, 0.5, 0.7],
-                [0.5, 0.5, 0.7]])
-h_ca_1d =  np.array([[0, 0, 0],
-                [0, 0, 0]])
+                    [0.5, 0.5, 0.7]])
+h_ca_1d = np.array([[0, 0, 0],
+                    [0, 0, 0]])
 h_ca_2u = np.array([[0, 0, 0],
-                [0, 0, 0]])
+                    [0, 0, 0]])
 h_ca_2d = np.array([[0, 0, 0],
-                [0, 0, 0]])
+                    [0, 0, 0]])
 h_ca_3u = np.array([[0, 0, 0],
-                [0, 0, 0]])
+                    [0, 0, 0]])
 h_ca_3d = np.array([[0, 0, 0],
-                [0, 0, 0]])
+                    [0, 0, 0]])
 
 h00 = np.zeros((8, 8))
 h00[0:2, 0:2] = np.array([[e_Cr_xy, 0], [0, e_Cr_x2_y2]])
@@ -49,7 +49,6 @@ h_11[0:2, 2:5] = h_ca_3u
 h = [h00, h_01, h_m10, h_01, h_11]
 
 
-
 def create_CrAs2(h):
     [h00, h_10, h_m10, h_01, h_11] = h
     sq3 = np.sqrt(3)
@@ -57,79 +56,78 @@ def create_CrAs2(h):
     a2 = np.array([-1/2, sq3/2, 0])
     a3 = np.array([0, 0, 2/3])
     lat = [list(a1), list(a2), list(a3)]
-    site_pos = [[0, 0, 0], # cr1
-            [-1/3, -1/6, 1/2],  # As+
-            [1/6, -1/6, -1/2]]  # As-
+    site_pos = [[0, 0, 0],  # cr1
+                [-1/3, -1/6, 1/2],  # As+
+                [1/6, -1/6, -1/2]]  # As-
     orb = [site_pos[0],
-        site_pos[0],
-        site_pos[1],
-        site_pos[1],
-        site_pos[1],
-        site_pos[2],
-        site_pos[2],
-        site_pos[2]]
+           site_pos[0],
+           site_pos[1],
+           site_pos[1],
+           site_pos[1],
+           site_pos[2],
+           site_pos[2],
+           site_pos[2]]
     tb_model = pytb.tb_model(2, 3, lat=lat, orb=orb, nspin=2)
     for i in range(8):
         tb_model.set_onsite(h00[i, i], i)
     for i in range(8):
         for j in range(8):
             if i < j:
-                if not(np.abs(h00[i, j])<1e-5):
+                if not(np.abs(h00[i, j]) < 1e-5):
                     tb_model.set_hop(h00[i, j], i, j, [0, 0, 0])
-                if not(np.abs(h_10[i, j])<1e-5):
+                if not(np.abs(h_10[i, j]) < 1e-5):
                     tb_model.set_hop(h_10[i, j], i, j, [1, 0, 0])
-                if not(np.abs(h_m10[i, j])<1e-5):
+                if not(np.abs(h_m10[i, j]) < 1e-5):
                     tb_model.set_hop(h_m10[i, j], i, j, [-1, 0, 0])
-                if not(np.abs(h_01[i, j])<1e-5):
+                if not(np.abs(h_01[i, j]) < 1e-5):
                     tb_model.set_hop(h_01[i, j], i, j, [0, -1, 0])
-                if not(np.abs(h_11[i, j])<1e-5):
+                if not(np.abs(h_11[i, j]) < 1e-5):
                     tb_model.set_hop(h_11[i, j], i, j, [1, -1, 0])
     return tb_model
 
 
-
 model = create_CrAs2(h)
 
-path=[[0.,0.],[2./3.,1./3.],[.5,.5],[1./3.,2./3.], [0.,0.]]
+path = [[0., 0.], [2./3., 1./3.], [.5, .5], [1./3., 2./3.], [0., 0.]]
 # labels of the nodes
-label=(r'$\Gamma $',r'$K$', r'$M$', r'$K^\prime$', r'$\Gamma $')
+label = (r'$\Gamma $', r'$K$', r'$M$', r'$K^\prime$', r'$\Gamma $')
 
 # call function k_path to construct the actual path
-(k_vec,k_dist,k_node) = model.k_path(path,301)
+(k_vec, k_dist, k_node) = model.k_path(path, 301)
 
 evals = model.solve_all(k_vec)
 
-#Plot of bands
+# Plot of bands
 fig, ax = plt.subplots()
-ax.set_xlim(k_node[0],k_node[-1])
+ax.set_xlim(k_node[0], k_node[-1])
 ax.set_xticks(k_node)
 ax.set_xticklabels(label)
 for n in range(len(k_node)):
-  ax.axvline(x=k_node[n],linewidth=0.5, color='k')
+    ax.axvline(x=k_node[n], linewidth=0.5, color='k')
 ax.set_title("Tight binding model CrAs2")
 ax.set_xlabel("Path in k-space")
 ax.set_ylabel("Band energy")
 
 
-ax.plot(k_dist,evals[0])
-ax.plot(k_dist,evals[1])
-ax.plot(k_dist,evals[2])
-ax.plot(k_dist,evals[3])
+ax.plot(k_dist, evals[0])
+ax.plot(k_dist, evals[1])
+ax.plot(k_dist, evals[2])
+ax.plot(k_dist, evals[3])
 
-ax.plot(k_dist,evals[4]) # banda cte baja
-ax.plot(k_dist,evals[5]) #banda cte baja
+ax.plot(k_dist, evals[4])  # banda cte baja
+ax.plot(k_dist, evals[5])  # banda cte baja
 
-ax.plot(k_dist,evals[6])
-ax.plot(k_dist,evals[7])
-ax.plot(k_dist,evals[8])
-ax.plot(k_dist,evals[9])
+ax.plot(k_dist, evals[6])
+ax.plot(k_dist, evals[7])
+ax.plot(k_dist, evals[8])
+ax.plot(k_dist, evals[9])
 
-ax.plot(k_dist,evals[10]) # banda cte alta
-ax.plot(k_dist,evals[11]) # banda cte alta
-ax.plot(k_dist,evals[12])
-ax.plot(k_dist,evals[13])
-ax.plot(k_dist,evals[14])
-ax.plot(k_dist,evals[15])
+ax.plot(k_dist, evals[10])  # banda cte alta
+ax.plot(k_dist, evals[11])  # banda cte alta
+ax.plot(k_dist, evals[12])
+ax.plot(k_dist, evals[13])
+ax.plot(k_dist, evals[14])
+ax.plot(k_dist, evals[15])
 ax.set_ylim(np.min(evals)-0.1, np.max(evals)+0.25)
 
 '''
