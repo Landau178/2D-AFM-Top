46009fd8c0045a2a6c8bf63afc522a410d3b45d3
Landau178 <vbjaeschke@gmail.com>
2020-10-09 19:20:01 -0300

    add atribute nband

--- sim_tb.py
+++ sim_tb.py
@@ -68,6 +68,8 @@ class Simulation_TB():
         self.lat = np.array(config["lat"])
         self.set_recip_lat()
         self.orb = config["orb"]
+        self.nspin = config["nspin"]
+        self.nband = len(self.orb) * self.nspin
         self.k_spoints = config["k_spoints"]
         self.k_sp_labels = config["k_sp_labels"]
 
