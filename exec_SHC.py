f729bf7dd0521045780ec6e37bc3e75e52bcb8ab
Landau178 <vbjaeschke@gmail.com>
2021-01-11 12:00:09 +0100

    Add charge_conductivity calculation to script. Mode "c"

--- exec_SHC.py
+++ exec_SHC.py
@@ -27,22 +27,31 @@ Parser.add('-a', default=0, action='store',
            type=int, help="Direction of the current")
 Parser.add('-b', default=0, action='store',
            type=int, help="Direction of the applied electric field")
+Parser.add("--mode", default="s", action="store", type=str,
+           help="s for spin conductivity, c for charge conductiviy.")
+
+
 options = Parser.parse_args()
 i = options.i
 a = options.a
 b = options.b
-mssg = "Calculation of the component\
+mode = options.mode
+
+mssg_s = "Calculation of the component\
      ({},{},{}) of the spin conductivity tensor.".format(i, a, b)
+mssg_c = "Calculation of the component\
+    ({},{}) of the charge conductivity tensor.".format(a, b)
+mssg = {"s": mssg_s, "c": mssg_c}[mode]
 print(mssg)
 # -----------------------------------------------------------------------------
 
-# Initi the simulation
+# Init the simulation
 t, J, t2 = 1.0, 1.7, 0.0
 mag_mode = "coplanar"
 path = toy.init_kagome_model(t, J, t2, mag_mode)
 Sim = stb.Simulation_TB(path)
 Sim.set_fermi_lvl()
-
+conductivity = {"s": Sim.spin_conductivity, "c": Sim.charge_conductivity}[mode]
 
 # Calculation of spin conductivity
 gamma_arr = np.logspace(-3, 0, num=50)
@@ -51,13 +60,17 @@ result = []
 for g in range(nG):
     print("Iteration {}".format(g))
     print("Using Gamma={}".format(gamma_arr[g]))
-    result.append(Sim.spin_conductivity(i, a, b, Gamma=gamma_arr[g]))
+    args = {"s": (i, a, b, gamma_arr[g]),
+            "c": (a, b, gamma_arr[g])}[mode]
+    result.append(conductivity(*args))
 
 # Saving result
 integ_result = np.zeros((nG, 3))
 integ_result[:, 0] = gamma_arr
 integ_result[:, 1:3] = np.array(result)
-path = Sim.path / "spin_conductivity/"
+path = {"s": Sim.path / "spin_conductivity/",
+        "c": Sim.path / "charge_conductivity/"}[mode]
 toy.mk_dir(path)
-name_shc = "SHC_{}{}{}.npy".format(i, a, b)
-np.save(path / name_shc, integ_result)
+name = {"s": "SHC_{}{}{}.npy".format(
+    i, a, b), "c": "CHC_{}{}.npy".format(a, b)}
+np.save(path / name, integ_result)
