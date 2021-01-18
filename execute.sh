b2fdd2e558b99a6977562963ae53d6351b656721
Landau178 <vbjaeschke@gmail.com>
2021-01-18 21:54:06 +0100

    modify execute.sh

--- execute.sh
+++ execute.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
 
-python3 exec_SHC.py -i 0 -a 1 -b 0
-python3 exec_SHC.py -i 1 -a 0 -b 0
-python3 exec_SHC.py -i 2 -a 1 -b 1
-python3 exec_SHC.py -i 2 -a 0 -b 0
-python3 exec_SHC.py -i 1 -a 1 -b 1
-python3 exec_SHC.py -i 0 -a 0 -b 1
+# python3 exec_SHC.py -i 0 -a 1 -b 0
+# python3 exec_SHC.py -i 1 -a 0 -b 0
+# python3 exec_SHC.py -i 2 -a 1 -b 1
+# python3 exec_SHC.py -i 2 -a 0 -b 0
+# python3 exec_SHC.py -i 1 -a 1 -b 1
+# python3 exec_SHC.py -i 0 -a 0 -b 1
+
+python3 exec_SHC.py -i 0 -a 0 -b 0
+python3 exec_SHC.py -i 1 -a 1 -b 0
+python3 exec_SHC.py -i 2 -a 1 -b 0
+python3 exec_SHC.py -i 2 -a 0 -b 1
+python3 exec_SHC.py -i 1 -a 0 -b 1
+python3 exec_SHC.py -i 0 -a 1 -b 1
 
 
-python3 exec_SHC.py -a 0 -b 0 --mode c
-python3 exec_SHC.py -a 1 -b 1 --mode c
+# python3 exec_SHC.py -a 0 -b 0 --mode c
+# python3 exec_SHC.py -a 1 -b 1 --mode c
+
+python3 exec_SHC.py -a 1 -b 0 --mode c
+python3 exec_SHC.py -a 0 -b 1 --mode c
 
 
 
