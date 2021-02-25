9058103ed9e18f5c86e9792be8fa422323e18b2a
Landau178 <vbjaeschke@gmail.com>
2021-02-25 20:14:22 +0100

    Updates in execute

--- execute.sh
+++ execute.sh
@@ -1,25 +1,27 @@
 #! /bin/bash
 
-python3 exec_SHC.py -i 0 -a 1 -b 0
-python3 exec_SHC.py -i 1 -a 0 -b 0
-python3 exec_SHC.py -i 2 -a 1 -b 1
-python3 exec_SHC.py -i 2 -a 0 -b 0
-python3 exec_SHC.py -i 1 -a 1 -b 1
-python3 exec_SHC.py -i 0 -a 0 -b 1
+opt_tr = "--tr even"
 
-python3 exec_SHC.py -i 0 -a 0 -b 0
-python3 exec_SHC.py -i 1 -a 1 -b 0
-python3 exec_SHC.py -i 2 -a 1 -b 0
-python3 exec_SHC.py -i 2 -a 0 -b 1
-python3 exec_SHC.py -i 1 -a 0 -b 1
-python3 exec_SHC.py -i 0 -a 1 -b 1
+python3 exec_SHC.py -i 0 -a 1 -b 0 $opt_tr
+python3 exec_SHC.py -i 1 -a 0 -b 0 $opt_tr
+python3 exec_SHC.py -i 2 -a 1 -b 1 $opt_tr
+python3 exec_SHC.py -i 2 -a 0 -b 0 $opt_tr
+python3 exec_SHC.py -i 1 -a 1 -b 1 $opt_tr
+python3 exec_SHC.py -i 0 -a 0 -b 1 $opt_tr
+
+python3 exec_SHC.py -i 0 -a 0 -b 0 $opt_tr
+python3 exec_SHC.py -i 1 -a 1 -b 0 $opt_tr
+python3 exec_SHC.py -i 2 -a 1 -b 0 $opt_tr
+python3 exec_SHC.py -i 2 -a 0 -b 1 $opt_tr
+python3 exec_SHC.py -i 1 -a 0 -b 1 $opt_tr
+python3 exec_SHC.py -i 0 -a 1 -b 1 $opt_tr
 
 
-python3 exec_SHC.py -a 0 -b 0 --mode c
-python3 exec_SHC.py -a 1 -b 1 --mode c
+python3 exec_SHC.py -a 0 -b 0 --mode c $opt_tr
+python3 exec_SHC.py -a 1 -b 1 --mode c $opt_tr
 
-python3 exec_SHC.py -a 1 -b 0 --mode c
-python3 exec_SHC.py -a 0 -b 1 --mode c
+python3 exec_SHC.py -a 1 -b 0 --mode c $opt_tr
+python3 exec_SHC.py -a 0 -b 1 --mode c $opt_tr
 
 
 
