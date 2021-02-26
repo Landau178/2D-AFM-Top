02032fc245a0336e4c0433effb388602a82d3fc7
Landau178 <vbjaeschke@gmail.com>
2021-02-26 19:29:20 +0100

    fixxed issue in execute

--- execute_2.sh
+++ execute_2.sh
@@ -7,28 +7,24 @@ opt_tr="--tr even"
 program="python3 exec_conductivity.py $opt_tr $opt_soc"
 
 
+# Calcullate all components of spin conductivity
+$program -i 0 -a 1 -b 0
+$program -i 1 -a 0 -b 0
+$program -i 2 -a 1 -b 1
+$program -i 2 -a 0 -b 0
+$program -i 1 -a 1 -b 1
+$program -i 0 -a 0 -b 1
 
+$program -i 0 -a 0 -b 0
+$program -i 1 -a 1 -b 0
+$program -i 2 -a 1 -b 0
+$program -i 2 -a 0 -b 1
+$program -i 1 -a 0 -b 1
+$program -i 0 -a 1 -b 1
 
-
-$program -i 0 -a 1 -b
-$program -i 1 -a 0 -b
-$program -i 2 -a 1 -b
-$program -i 2 -a 0 -b
-$program -i 1 -a 1 -b
-$program -i 0 -a 0 -b
-
-$program -i 0 -a 0 -b
-$program -i 1 -a 1 -b
-$program -i 2 -a 1 -b
-$program -i 2 -a 0 -b
-$program -i 1 -a 0 -b
-$program -i 0 -a 1 -b
-
-
-$program -a 0 -b 0 --mode
-$program -a 1 -b 1 --mode
-
-$program -a 1 -b 0 --mode
-$program -a 0 -b 1 --mode
-
+#Calculate all components of charge conductivity
+$program -a 0 -b 0 --mode c
+$program -a 1 -b 1 --mode c
+$program -a 1 -b 0 --mode c
+$program -a 0 -b 1 --mode c
 
