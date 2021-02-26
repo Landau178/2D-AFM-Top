4fc97818e000c1faa7ee484d3d9fb63c85c68556
Landau178 <vbjaeschke@gmail.com>
2021-02-26 19:18:43 +0100

    Update to include --t2

--- execute_2.sh
+++ execute_2.sh
@@ -1,27 +1,33 @@
 #! /bin/bash
 
 opt_tr="--tr even"
-program="python3 exec_conductivity.py"
+opt_soc="--t2 0.0"
 
-$program -i 0 -a 1 -b 0 $opt_tr
-$program -i 1 -a 0 -b 0 $opt_tr
-$program -i 2 -a 1 -b 1 $opt_tr
-$program -i 2 -a 0 -b 0 $opt_tr
-$program -i 1 -a 1 -b 1 $opt_tr
-$program -i 0 -a 0 -b 1 $opt_tr
-
-$program -i 0 -a 0 -b 0 $opt_tr
-$program -i 1 -a 1 -b 0 $opt_tr
-$program -i 2 -a 1 -b 0 $opt_tr
-$program -i 2 -a 0 -b 1 $opt_tr
-$program -i 1 -a 0 -b 1 $opt_tr
-$program -i 0 -a 1 -b 1 $opt_tr
+program="python3 exec_conductivity.py $opt_tr $opt_soc"
 
 
-$program -a 0 -b 0 --mode c $opt_tr
-$program -a 1 -b 1 --mode c $opt_tr
 
-$program -a 1 -b 0 --mode c $opt_tr
-$program -a 0 -b 1 --mode c $opt_tr
+
+
+$program -i 0 -a 1 -b
+$program -i 1 -a 0 -b
+$program -i 2 -a 1 -b
+$program -i 2 -a 0 -b
+$program -i 1 -a 1 -b
+$program -i 0 -a 0 -b
+
+$program -i 0 -a 0 -b
+$program -i 1 -a 1 -b
+$program -i 2 -a 1 -b
+$program -i 2 -a 0 -b
+$program -i 1 -a 0 -b
+$program -i 0 -a 1 -b
+
+
+$program -a 0 -b 0 --mode
+$program -a 1 -b 1 --mode
+
+$program -a 1 -b 0 --mode
+$program -a 0 -b 1 --mode
 
 
