a43922daa6bdc2361e4681ac70338a69223593f8
Landau178 <vbjaeschke@gmail.com>
2021-02-27 03:53:15 +0100

    update execute

--- execute.sh
+++ execute.sh
@@ -1,27 +1,28 @@
 #! /bin/bash
 
+opt_soc="--t2 $1"
 opt_tr="--tr even"
+opt_mag="--mag_mode coplanar_b"
 
-python3 exec_SHC.py -i 0 -a 1 -b 0 $opt_tr
-python3 exec_SHC.py -i 1 -a 0 -b 0 $opt_tr
-python3 exec_SHC.py -i 2 -a 1 -b 1 $opt_tr
-python3 exec_SHC.py -i 2 -a 0 -b 0 $opt_tr
-python3 exec_SHC.py -i 1 -a 1 -b 1 $opt_tr
-python3 exec_SHC.py -i 0 -a 0 -b 1 $opt_tr
+program="python3 exec_conductivity.py $opt_tr $opt_soc $opt_mag"
 
-python3 exec_SHC.py -i 0 -a 0 -b 0 $opt_tr
-python3 exec_SHC.py -i 1 -a 1 -b 0 $opt_tr
-python3 exec_SHC.py -i 2 -a 1 -b 0 $opt_tr
-python3 exec_SHC.py -i 2 -a 0 -b 1 $opt_tr
-python3 exec_SHC.py -i 1 -a 0 -b 1 $opt_tr
-python3 exec_SHC.py -i 0 -a 1 -b 1 $opt_tr
+# Calcullate all components of spin conductivity
+$program -i 0 -a 1 -b 0
+$program -i 1 -a 0 -b 0 --load
+$program -i 2 -a 1 -b 1 --load
+$program -i 2 -a 0 -b 0 --load
+$program -i 1 -a 1 -b 1 --load
+$program -i 0 -a 0 -b 1 --load
 
+$program -i 0 -a 0 -b 0 --load
+$program -i 1 -a 1 -b 0 --load
+$program -i 2 -a 1 -b 0 --load
+$program -i 2 -a 0 -b 1 --load
+$program -i 1 -a 0 -b 1 --load
+$program -i 0 -a 1 -b 1 --load
 
-python3 exec_SHC.py -a 0 -b 0 --mode c $opt_tr
-python3 exec_SHC.py -a 1 -b 1 --mode c $opt_tr
-
-python3 exec_SHC.py -a 1 -b 0 --mode c $opt_tr
-python3 exec_SHC.py -a 0 -b 1 --mode c $opt_tr
-
-
-
+#Calculate all components of charge conductivity
+$program -a 0 -b 0 --mode c --load
+$program -a 1 -b 1 --mode c --load
+$program -a 1 -b 0 --mode c --load
+$program -a 0 -b 1 --mode c --load
