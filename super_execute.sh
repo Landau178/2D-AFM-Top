5805a99600127060a60e6e40848389fb7baae08d
Landau178 <vbjaeschke@gmail.com>
2021-03-07 00:59:29 +0100

    add super_execute

---
+++ super_execute.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+./execute.sh 0.02 &
+./execute.sh 0.04 &
+./execute.sh 0.06 &
+./execute.sh 0.08 &
+./execute.sh 0.10 &
+./execute.sh 0.12 &
+./execute.sh 0.14 &
+./execute.sh 0.16 &
+./execute.sh 0.18 &
+
