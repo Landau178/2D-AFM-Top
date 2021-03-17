2eb83022bf47124f3cadab2652688bca3188deea
Landau178 <vbjaeschke@gmail.com>
2021-03-17 15:11:41 +0100

    Reorganized Program in main

--- rashba_spin_cond.py
+++ rashba_spin_cond.py
@@ -17,43 +17,63 @@ import analytical_models as amod
 # This script calculates and save the nonzero components of the spin/charge
 # conductivity in the Rashba Hamiltonian.
 # -----------------------------------------------------------------------------
-mode = "c"
-nproc = 8
-# -----------------------------------------------------------------------------
-# Simulation parameters
-# -----------------------------------------------------------------------------
-alpha, B = 0.295, 0.1
-lamb = 0.0
-th, phi = np.pi/2, 0
-path = amod.create_path_rashba_model("", alpha, B=B, th=th, phi=phi, lamb=lamb)
-Sim_rash = amod.Rashba_model(path, alpha, B=B, th=th, phi=phi, lamb=lamb)
-# -----------------------------------------------------------------------------
 
-nk = 400
-nE = 50
+
+def main():
+    # -----------------------------------------------------------------------------
+    # Routine parameters
+    # -----------------------------------------------------------------------------
+    mode = "c"
+    nproc = 6
+
+    nk = 400
+    nE = 50
+
+    # -----------------------------------------------------------------------------
+    # Simulation parameters
+    # -----------------------------------------------------------------------------
+    alpha, B = 0.295, 0.1
+    lamb = 0.0
+    th, phi = np.pi/2, 0
+    path = amod.create_path_rashba_model(
+        "", alpha, B=B, th=th, phi=phi, lamb=lamb)
+    Sim_rash = amod.Rashba_model(path, alpha, B=B, th=th, phi=phi, lamb=lamb)
+    # -----------------------------------------------------------------------------
+    Gamma_arr = np.array([12.7e-3])
+    mode_c_list = ["odd_z", "even_z", "odd_m", "even_m"]
+    mode_s_list = ["zelezny", "mook"]
+
+    if mode == "s":
+        routine_spin_cond(Sim_rash, Gamma_arr, mode_s_list, nk, nE, nproc)
+
+    elif mode == "c":
+        routine_charge_cond(Sim_rash, Gamma_arr, mode_c_list, nk, nE, nproc)
+
+
 # np.array([20e-3, 30e-3, 40e-3, 50e-3]) # 1e-3
-Gamma_arr = np.array([12.7e-3])
-mode_c_list = ["odd_z", "even_z", "odd_m", "even_m"]
-mode_s_list = ["zelezny", "mook"]
+
+
+def routine_spin_cond(Sim_rash, Gamma_arr, mode_s_list, nk, nE, nproc):
+    for Gamma in Gamma_arr:
+        for mode_s in mode_s_list:
+            mssg = "Calculating spin conductivity: {}-{}"
+            opts = {"nk": nk, "nE": nE, "mode": mode_s, "nproc": nproc}
+            print(mssg.format(mode_s, "yxy"))
+            Sim_rash.spin_conductivity_vs_Ef((1, 0, 1), Gamma, **opts)
+            print(mssg.format(mode_s, "yyx"))
+            Sim_rash.spin_conductivity_vs_Ef((1, 1, 0), Gamma, **opts)
+
+
+def routine_charge_cond(Sim_rash, Gamma_arr, mode_c_list, nk, nE, nproc):
+    for Gamma in Gamma_arr:
+        for mode_c in mode_c_list:
+            mssg = "Calculating charge conductivity: {}-{}"
+            opts = {"nk": nk, "nE": nE, "mode": mode_c, "nproc": nproc}
+            print(mssg.forma(mode_c, "xx"))
+            Sim_rash.charge_conductivity_vs_Ef((0, 0), Gamma, **opts)
+            print(mssg.forma(mode_c, "yy"))
+            Sim_rash.charge_conductivity_vs_Ef((1, 1), Gamma, **opts)
 
 
 if __name__ == "__main__":
-    if mode == "s":
-        for Gamma in Gamma_arr:
-            for mode_s in mode_s_list:
-                mssg = "Calculating spin conductivity: {}-{}"
-                opts = {"nk": nk, "nE": nE, "mode": mode_s, "nproc": nproc}
-                print(mssg.format(mode_s, "yxy"))
-                Sim_rash.spin_conductivity_vs_Ef((1, 0, 1), Gamma, **opts)
-                print(mssg.format(mode_s, "yyx"))
-                Sim_rash.spin_conductivity_vs_Ef((1, 1, 0), Gamma, **opts)
-
-    elif mode == "c":
-        for Gamma in Gamma_arr:
-            for mode_c in mode_c_list:
-                mssg = "Calculating charge conductivity: {}-{}"
-                opts = {"nk": nk, "nE": nE, "mode": mode_c, "nproc": nproc}
-                print(mssg.forma(mode_c, "xx"))
-                Sim_rash.charge_conductivity_vs_Ef((0, 0), Gamma, **opts)
-                print(mssg.forma(mode_c, "yy"))
-                Sim_rash.charge_conductivity_vs_Ef((1, 1), Gamma, **opts)
+    main()
